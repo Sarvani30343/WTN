573dc91910c6bad2a5d2cabb47f316643f902669
mylaptop <mylaptop@DESKTOP-2QOB21C>
2020-06-11 14:05:02 +0530

    committed to git

---
+++ WTNdemo/src/enhancedfor.java
@@ -0,0 +1,15 @@
+
+public class enhancedfor {
+	public static void main(String[] args) {
+		int a[]= {1,2,3};
+	/*	for(int i=0;i<a.length;i++) {
+			System.out.println(a[i]);
+		}*/
+		
+		//enhanced for loop or for each loop usage is down
+		for(int i:a) {
+			System.out.println(i);
+		}
+	}
+
+}
