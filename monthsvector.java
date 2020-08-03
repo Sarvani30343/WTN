9f4d5639c238813ed946e7fc37e0526483e6a017
Sarvani30343 <56549235+Sarvani30343@users.noreply.github.com>
2020-08-03 12:47:23 +0530

    collections

---
+++ monthsvector.java
@@ -0,0 +1,26 @@
+package collections;
+
+import java.util.*;
+
+public class monthsvector {
+public static void main(String[] args) {
+	Vector v = new Vector();   
+     v.add("Jan");
+     v.add("Feb");
+     v.add("Mar");
+     v.add("Apr");
+     v.add("May");
+     v.add("Jun");
+     v.add("Jul"); 
+     v.add("Aug");
+     v.add("Sep");
+     v.add("Oct");
+     v.add("Nov");
+     v.add("Dec");
+     
+ 
+    
+     System.out.println(v);
+ 
+}
+}
